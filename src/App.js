63c7c034ad2649de200f8fd2bb3822b7c6450097
LN27100 <heloceinlove@hotmail.fr>
2023-11-08 13:28:56 +0100

    fin word 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <img src="/images/dwwm_1.png" className="App-logo" alt="logo" />       
+       <p>
+          TP React Développeur Web et Web Mobile
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Rechercher des films
         </a>
       </header>
     </div>
