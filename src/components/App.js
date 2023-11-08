05fd34d4c056fd59a6c589130de4cccb1c29bdff
LN27100 <heloceinlove@hotmail.fr>
2023-11-08 15:30:24 +0100

    fin word 3

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,10 @@ import Home from './Home/Home';
 
 function App() {
 
-  const clicBouton = () => alert('bouton cliqué')
 
   return (
     <div className="App">
       <Home />
-
-      <button className="App-button" onClick={ clicBouton }>Modifier l’animation</button>
-
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './Home.css'
 
-const Home = () => {
+function Home() {
+    const [animation, setAnimation] = useState(true)
+
+    const toggleAnimation = () => {
+        console.log(animation)
+        setAnimation(animation => !animation)
+    }
+   
     return (
         <div className="container">
-            <img src={"../images/dwwm_1.png"} className="logo" alt="logo" />
+            <img src={"../images/dwwm_1.png"} className={`logo ${animation && "rotate"}`} alt="logo" />
             <h1>TP React Développeur Web et Web Mobile</h1>
+            <div className="App">
+                
+                <button className="App-button" onClick={toggleAnimation}>Modifier l’animation</button>
+
+            </div>
         </div>
-    )}
-    export default Home
-  
+    )
+}
+export default Home
