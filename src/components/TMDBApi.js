7e01b94941fec17d19b6d4a26e88ed62a6a88cca
LN27100 <heloceinlovehotmail.fr>
2023-11-09 16:53:05 +0100

    suite exo word 6 avec debugg

--- src/components/TMDBApi.js
+++ src/components/TMDBApi.js
@@ -1,12 +1,17 @@
-const API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjU1OTczNjhjODc0ZmZkODQ3M2EwYTc1YjUxYzc0NCIsInN1YiI6IjY1M2ZhNTkzY2M5NjgzMDEwYjNmZmIxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Adh4V1F62MoI7ZhKTQvjGYO_Z3OTU3p6xhV-KwWIE4Q";
-
 //le paramètre text = film cherché, page = nombre de page voulu
+
 export function getFilmsFromTMdbWithText(text, page) {
 
-  const url = "https://api.themoviedb.org/3/search/movie?api_key=" + API_TOKEN +
-    "&language=fr&query=" + text + '&page=' + page;
-
-  return fetch(url)
-    .then((response) => response.json)
-    .catch((error) => console.log(error))
-}
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTdjYzc0OTU1MTQ5YmUyM2RmODM4MTNmMjAxYTRlOCIsInN1YiI6IjYyODM5OGJiZWM0NTUyMTAzMmE5NTcxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.REF4Oi-K06F7Jq8LolG5vPQtyeiGk3nBFdDyL1FLq7E'
+    }
+  };
+  
+  
+    return fetch(`https://api.themoviedb.org/3/search/movie?query=${text}&include_adult=false&language=fr-FR&page=${page}`, options)
+      .then((response) => response.json())
+      .catch((error) => console.log(error))
+  }
